665949fe14bed367021cb69864a221bb58f038a8
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-05-16 13:30:33 -0400

    CHALLENGE 2: ARRAY CHUNKING

--- session2/script.js
+++ session2/script.js
@@ -21,4 +21,50 @@ function logestWord(sentence) {
     return longestWordArr;
   }
 }
+
+///////////////////////////////////
+//CHALLENGE 2: ARRAY CHUNKING
+//split an array into chunked arrays of a specific length
+function chunkArray(arr, len) {
+  // //way 1: with while loop
+  // //init chunked arr
+  // const chunkedArr = [];
+  // //set index
+  // let i = 0;
+  // //loop while index is less than the array length(len)
+  // while (i < arr.length) {
+  //   //Slice out from the index to the index + len and push onto the chunkedArr
+  //   chunkedArr.push(arr.slice(i, i + len));
+  //   //Increment by chunk length
+  //   i += len;
+  // }
+  // return chunkedArr;
+  //////////
+  //way 2: with forEach() method
+  //init chunked arr
+  const chunkedArr = [];
+  // loop through arr
+  arr.forEach((val) => {
+    //get last element
+    const last = chunkedArr[chunkedArr.length - 1];
+
+    //check if last and if last length is equal to the chunk length(len)
+    if (!last || last.length === len) {
+      chunkedArr.push([val]);
+    } else {
+      last.push(val);
+    }
+  });
+  return chunkedArr;
+}
+
+//////////////////////////////////
+
+//////////////////////////////////
+//////////////////////////////////
+//functions calls
 console.log(logestWord("hello, world, hi"));
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 3));
+const arr1 = ["hello"];
+arr1.push([1]);
+console.log(arr1);
