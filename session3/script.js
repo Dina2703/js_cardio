75a7f29c8922d9daad4cec31123da5a0e25376dd
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-05-16 22:38:42 -0400

    session3: CHALLENGE 1: ADD ALL NUMBERS

---
+++ session3/script.js
@@ -0,0 +1,34 @@
+//CHALLENGE 1: ADD ALL NUMBERS
+//return a value of all parametersentered regarless of the amount of numbers - NO ARRAYS
+//ex addAll(2, 5, 7) === 14
+
+//SOLUTION 1: with ES5/old way, using arguments and for loop
+function addAll(numbers) {
+  //copy all numbers that passed in into an array.Ex: input: addAll(2, 5, 6), output: [ 2, 5, 6 ]
+  var args = Array.prototype.slice.call(arguments);
+  //add each number
+  let total = 0;
+  for (i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+}
+
+//SOLUTION 2: with ES6/ using ...rest operator & forOf & reduce() methods. ...numbers gathering the remaining parameters into an array. input:addAll2(2, 5, 6), output: [ 2, 5, 6 ]
+function addAll2(...numbers) {
+  //with for of loop
+  // let total = 0;
+  // for (number of numbers) {
+  //   total += number;
+  // }
+  // return total;
+  //////////
+  //with reduce()
+  return numbers.reduce((accumulator, currentNum) => accumulator + currentNum);
+}
+
+/////////////////////////////////////
+
+//function calls
+console.log(addAll(2, 5, 6));
+console.log(addAll2(2, 5, 6));
